37972a4e812ccb42cabfcad2a968b030ab10de18
Marc Gallant <marc.gallant@quanergy.com>
2017-05-23 10:41:06 -0400

    Added Doxygen documentation to header files.

--- include/euler/io.h
+++ include/euler/io.h
@@ -7,10 +7,32 @@
 
 namespace euler
 {
+  /**
+   * @brief Determines if a given sequence is a valid sequence of axes from
+   * which a rotation matrix and unit quaternion can be calculated.
+   *
+   * @param[in] sequence A string that potentially represents a rotation
+   * sequence
+   *
+   * @return True if the input sequence is a valid rotation sequence, false
+   * otherwise.
+   */
   bool isSequenceValid(const Sequence& sequence);
 
+  /**
+   * @brief Prints a rotation matrix to the console nicely (i.e., reasonable
+   * number of decimal places, columns are aligned).
+   *
+   * @param[in] R The rotation matrix to print.
+   */
   void prettyPrint(const RotationMatrix& R);
 
+  /**
+   * @brief Print a quaternion to the console nicely (i.e., reasonable number of
+   * decimal places, the name of each element is given).
+   *
+   * @param[in] q The quaternion to print.
+   */
   void prettyPrint(const Quaternion& q);
 } // namespace euler
 
--- include/euler/rotations.h
+++ include/euler/rotations.h
@@ -8,23 +8,39 @@
 
 namespace euler
 {
+  /// Rotation sequence (e.g., xyz, zxz, yxy, etc.)
   using Sequence = std::string;
+
+  /// Euler angles in the same order as the sequence
   using Angles = std::array<double, 3>;
+
+  /// Always orthnonormal 3x3 rotation matrix
   using RotationMatrix = Eigen::Matrix3d;
+
+  /// Always normalized (i.e., unit quaternion)
   using Quaternion = Eigen::Quaterniond;
 
+  /** 
+   * @brief Order of rotation
+   */
   enum class Order
   {
     INTRINSIC,
     EXTRINSIC
   };
 
+  /** 
+   * @brief Direction of rotation
+   */
   enum class Direction
   {
     ACTIVE,
     PASSIVE
   };
 
+  /** 
+   * @brief Fully describes the conventions used for a rotation 
+   **/
   struct Convention
   {
     Convention(Order o, Direction d) : order(o), direction(d){};
@@ -32,14 +48,49 @@ namespace euler
     const Direction direction;
   };
 
+  /**
+   * @brief Calculates a rotation matrix.
+   *
+   * @param[in] sequence The sequence of principal axes (e.g., xyz, zxz, etc.)
+   * @param[in] angles The three Euler angles in the same order as the sequence
+   * @param[in] convention The convention used to generate the rotation matrix
+   *
+   * @return The resulting rotation matrix
+   */
   RotationMatrix getRotationMatrix(const Sequence& sequence,
                                    const Angles& angles, Convention convention);
 
+  /**
+   * @brief Converts a quaternion to a rotation matrix. Normalizes the
+   * quaternion first.
+   *
+   * @param[in] q A quaternion representing a rotation
+   *
+   * @return A rotation matrix representing the same rotation as the input
+   * quaternion
+   */
   RotationMatrix getRotationMatrix(const Quaternion& q);
 
+  /** 
+   * @brief Calculates a unit quaternion representing a rotation.
+   * 
+   * @param[in] sequence The sequence of principal axes (e.g., xyz, zxz, etc.)
+   * @param[in] angles The three Euler angles in the same order as the sequence
+   * @param[in] convention The convention used to generate the rotation matrix
+   * 
+   * @return The resulting unit quaternion
+   */
   Quaternion getQuaternion(const Sequence& sequence, const Angles& angles,
                            Convention convention);
 
+  /**
+   * @brief Converts a rotation matrix to a unit quaternion.
+   *
+   * @param[in] R A rotation matrix
+   *
+   * @return A unit quaternion representing the same rotation as the input
+   * rotation matrix
+   */
   Quaternion getQuaternion(const RotationMatrix& R);
 }  // namespace euler
 
